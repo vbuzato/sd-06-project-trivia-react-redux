634b0c9071901d72bd2b9eb677b7e08829e5a868
labelle <beelsdelonge@gmail.com>
2020-11-04 18:20:53 -0300

    [GRUPO 24] Passando 1 Requisito

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import logo from './trivia.png';
-import './App.css';
+import Login from './Pages/Login';
+import { Route, Switch } from 'react-router-dom';
 
-export default function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          SUA VEZ
-        </p>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  render(){
+    return (
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={ Login } />
+        </Switch>
+      </div>
+    );
+  }
 }
+
+export default App;
