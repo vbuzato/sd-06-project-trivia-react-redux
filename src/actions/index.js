e53344923264c143431b20c1f891cfc76a314a44
Vinicius Buzato <viniciusbuzato@gmail.com>
2020-11-05 17:19:44 -0300

    [GRUPO-24] re2 Concluído

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,8 @@
 export const ADD_USER = 'ADD_USER';
 export const USER_HASH = 'USER_HASH';
 export const USER_TOKEN = 'USER_TOKEN';
-export { fetchToken } from './requestAPI';
+
+const tokenAPI = 'https://opentdb.com/api_token.php?command=request';
 
 export const userInfo = (name, email) => ({
   type: ADD_USER,
@@ -13,3 +14,22 @@ export const userHash = (hash) => ({
   type: USER_HASH,
   hash,
 });
+
+// const avatarGravatar = 'https://www.gravatar.com/avatar/';
+
+// export const avatar = hash =>
+//   fetch(`${avatarGravatar}${hash}`).then(response =>
+//     response.json().then(result => (result.ok ? Promise.resolve(result) : Promise.reject(result))),
+//   );
+
+export const userToken = (token) => ({
+  type: USER_TOKEN,
+  token,
+});
+
+export const fetchToken = () => async (dispatch) => {
+  const requestToken = await fetch(tokenAPI);
+  const token = await requestToken.json();
+  localStorage.setItem('token', token.token);
+  return dispatch(userToken(token.token));
+};
