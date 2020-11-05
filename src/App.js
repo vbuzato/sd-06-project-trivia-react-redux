5d12b4787a92fc0a66703eb5a80e7e80881e4a7f
Alexandre Fogaça <alexandr.fogaca@gmail.com>
2020-11-05 18:48:03 -0300

    Dando merge nas branchs re2, 4 e 5

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Settings from './pages/Settings';
+import Questions from './pages/Questions';
 
 class App extends React.Component {
   render() {
@@ -9,6 +10,7 @@ class App extends React.Component {
       <Switch>
         <Route path="/settings" component={ Settings } />
         <Route exact path="/" component={ Login } />
+        <Route path="/game" component={ Questions } />
       </Switch>
     );
   }
