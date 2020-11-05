8eaf2b6db0982ac69c02fede0fd6b891ecb244e0
Alexandre Fogaça <alexandr.fogaca@gmail.com>
2020-11-05 16:37:00 -0300

    Criando um botao de voltar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import Settings from './pages/Settings';
 
 class App extends React.Component {
   render() {
     return (
       <Switch>
+        <Route path="/settings" component={ Settings } />
         <Route exact path="/" component={ Login } />
       </Switch>
     );
--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Settings extends React.Component {
   render() {
     return (
       <div>
         <h1 data-testid="settings-title">CONFIGURAÇÕES</h1>
-        <p>funfou mané!!</p>
+        <Link to="/"><button>Voltar</button></Link>
       </div>
     )
   }
