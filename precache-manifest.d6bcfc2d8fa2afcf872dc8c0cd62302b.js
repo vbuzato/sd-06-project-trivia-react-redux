c21160067974814a58dabc5884c4cbb24784b574
Vinicius Buzato <viniciusbuzato@gmail.com>
2021-01-03 20:25:54 -0300

    Updates

---
+++ precache-manifest.d6bcfc2d8fa2afcf872dc8c0cd62302b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a4b52b9aa37e8025c7ce53bde0a79ad",
+    "url": "/sd-06-project-trivia-react-redux/index.html"
+  },
+  {
+    "revision": "3ed04cd53e8a7fa5cdb5",
+    "url": "/sd-06-project-trivia-react-redux/static/css/main.4fef7628.chunk.css"
+  },
+  {
+    "revision": "c811d53eeb2050802fac",
+    "url": "/sd-06-project-trivia-react-redux/static/js/2.bcfba823.chunk.js"
+  },
+  {
+    "revision": "b9da430482e3f0eb230634f9fc6c20c5",
+    "url": "/sd-06-project-trivia-react-redux/static/js/2.bcfba823.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3ed04cd53e8a7fa5cdb5",
+    "url": "/sd-06-project-trivia-react-redux/static/js/main.80d0e06b.chunk.js"
+  },
+  {
+    "revision": "e3cc67917592077fef59",
+    "url": "/sd-06-project-trivia-react-redux/static/js/runtime-main.c44a877d.js"
+  },
+  {
+    "revision": "dd07c461fe33761d12074252304c012c",
+    "url": "/sd-06-project-trivia-react-redux/static/media/007-login.dd07c461.mp3"
+  },
+  {
+    "revision": "f14e07e7aa37027f0c1bda70af8785db",
+    "url": "/sd-06-project-trivia-react-redux/static/media/007-questions.f14e07e7.mp3"
+  },
+  {
+    "revision": "377467634adfe7b3ea89a638e2ebdc4e",
+    "url": "/sd-06-project-trivia-react-redux/static/media/feedback.37746763.mp3"
+  },
+  {
+    "revision": "58607b10f385ceb484c82b3803ef6862",
+    "url": "/sd-06-project-trivia-react-redux/static/media/loading.58607b10.gif"
+  },
+  {
+    "revision": "ec3823113a722edea2fde813c5b21c52",
+    "url": "/sd-06-project-trivia-react-redux/static/media/ranking.ec382311.mp3"
+  }
+]);
